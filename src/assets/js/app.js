2185b3266be49f1155472309ddc6da25e7ac6de7
Trabajo <Trabajo>
2016-08-12 18:14:44 +0200

    Añado todos los elementos obligatorios al listado de artículos, a falta de aplicar estilos

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,10 @@
 $(document).foundation();
 
+//Updates time tags continously
+jQuery("time").timeago();
+
+//Retrieves comments count per article
+commentsListManager.getCount();
+
 //loads comments list when they appear in viewport
 commentsListManager.showWhenVisible();
