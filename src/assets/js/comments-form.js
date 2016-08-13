f6dd162ae25000f4f7351aba0351f0e9064823ac
Trabajo <Trabajo>
2016-08-13 17:57:22 +0200

    Añado mensajes relativos a la carga correcta/incorrecta del listado de comentarios

--- src/assets/js/comments-form.js
+++ src/assets/js/comments-form.js
@@ -38,18 +38,6 @@ $('.new-comment-form').on("submit", function(){
         return false;
     }
 
-    /*
-    var audio_file_input = $("#audio_file")[0];
-    var audio_file = null;
-    if (audio_file_input.file != null) {
-        audio_file = audio_file_input.file;
-    }
-
-    var cover_file_input = $("#cover_file")[0];
-    var cover_file = null;
-    if (cover_file_input.file) { // igual que  cover_file_input.file != null
-        cover_file = cover_file_input.file;
-    }*/
     var avatar = null;
     var datetime = null;
     //2016-07-26T01:19+02:00
@@ -80,11 +68,12 @@ $('.new-comment-form').on("submit", function(){
     //send to api
     apiClientComments.save(comment, function(response) {
         $(".new-comment-form")[0].reset();
-        $("#form-messages").html("");
+        utils.hideError("#form-messages");
         commentsListManager.load(articleId);
         unsetLoading();
     }, function() {
-        console.error("ERROR", arguments);
+        utils.showError("No se ha podido enviar el comentario. Prueba de nuevo en unos instantes.", "#form-messages");
+        //console.error("ERROR", arguments);
         unsetLoading();
     });
 
--- src/assets/js/comments-list-manager.js
+++ src/assets/js/comments-list-manager.js
@@ -2,34 +2,42 @@ var commentsListManager = (function() {
 
 	return {
 		load: function(articleId) {
+			$('.comments-list').html('<p>Cargando comentarios. Un momento por favor.</p>'); //vaciamos la lista
 
 			apiClientComments.list(articleId, function(response) {
-				$('.comments-list').html(''); //vaciamos la lista
-				for (var i in response) {
-					var comment = response[i];
+				
+				if (response.length>0) {
+					$('.comments-list').html("");
+					for (var i in response) {
+						var comment = response[i];
 
-					var avatar_url = comment.avatar_url || "";
-					if (avatar_url == "") {
-						avatar_url = 'assets/img/comment-autor-placeholder.png';
+						var avatar_url = comment.avatar_url || "";
+						if (avatar_url == "") {
+							avatar_url = 'assets/img/comment-autor-placeholder.png';
+						}
+
+						var html = '<div class="row">';
+						html += '<article class="comment">';
+						html += '<header>';
+						html += '<img class="avatar" src="'+ avatar_url + '">';
+						html += '<div class="commenter">Por <a title="Enviar e-mail" href="mailto:' + utils.escapeHTML(comment.name) + '">' + utils.escapeHTML(comment.name) + ' ' + utils.escapeHTML(comment.surname) + '</a> <i>(<time class="comment_date" datetime="' + comment.datetime + '">' + utils.simplifyDates(comment.datetime) + '</time>)</i>:</div>';
+						html += '</header>';
+						html += '<div class="comment_text">' + utils.crlf2br(utils.escapeHTML(comment.comment)) + '</div>';
+						html += '</article>';
+						html += '</div>';
+
+						
+						$('.comments-list').append(html);					
 					}
-
-					var html = '<div class="row">';
-					html += '<article class="comment">';
-					html += '<header>';
-					html += '<img class="avatar" src="'+ avatar_url + '">';
-					html += '<div class="commenter">Por <a title="Enviar e-mail" href="mailto:' + utils.escapeHTML(comment.name) + '">' + utils.escapeHTML(comment.name) + ' ' + utils.escapeHTML(comment.surname) + '</a> <i>(<time class="comment_date" datetime="' + comment.datetime + '">' + utils.simplifyDates(comment.datetime) + '</time>)</i>:</div>';
-					html += '</header>';
-					html += '<div class="comment_text">' + utils.crlf2br(utils.escapeHTML(comment.comment)) + '</div>';
-					html += '</article>';
-					html += '</div>';
-
-					$('.comments-list').append(html);					
-				}
+				} else {
+					$('.comments-list').html('<p>Aún no hay ningún comentario sobre este artículo. ¡Puedes ser el primero!</p>');
+				}				
 
 				jQuery("time").timeago();
 
 			}, function(response) {
-				console.error("ERROR LISTING", response);
+				$('.comments-list').html('No se han podido cargar los comentarios. Intenta recargar la página pasados unos instantes.');
+				//console.error("ERROR LISTING", response);
 			});
 		},
 
